fe02e1173215f87d7d2b020d8c24267757ff6aae
Damien <damien.milacki@gmail.com>
2026-01-28 11:21:26 +1100

    update tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in {100..600..100}; do
-  ./build/lifelong --inputFile ./instances/warehouseSmall/warehouseSmall_${i}.json -o outputs/ws{$i}.json --scheduleModel 6 --simulationTime 1000 --networkTimeSteps 10
+  ./build/lifelong --inputFile ./instances/warehouseSmall/warehouseSmall_${i}.json -o outputs/ws${i}.json --scheduleModel 6 --simulationTime 1000 --networkTimeSteps 10
 done
